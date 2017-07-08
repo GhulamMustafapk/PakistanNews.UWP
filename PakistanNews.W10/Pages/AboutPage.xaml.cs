f72d142b11bcb1522f0ade8fffe339b348046b89
Ghulam Mustafa <itsghulammustafa@outlook.com>
2017-07-08 14:26:53 +0500

    ..

--- PakistanNews.W10/Pages/AboutPage.xaml.cs
+++ PakistanNews.W10/Pages/AboutPage.xaml.cs
@@ -1,13 +1,3 @@
-//---------------------------------------------------------------------------
-//
-// <copyright file="AboutPage.xaml.cs" company="Microsoft">
-//    Copyright (C) 2015 by Microsoft Corporation.  All rights reserved.
-// </copyright>
-//
-// <createdOn>7/6/2017 5:33:38 AM</createdOn>
-//
-//---------------------------------------------------------------------------
-
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using PakistanNews.ViewModels;
